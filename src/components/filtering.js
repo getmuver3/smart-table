48fb4ebc514bb93f0b4f479141ba7b8d4427a883
HBB <getmuver3@yandex.ru>
2026-06-04 23:11:25 +0300

    реализовать умная таблица

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,42 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+import { createComparison, defaultRules } from '../lib/compare.js';
 
 // @todo: #4.3 — настроить компаратор
 
+const compare = createComparison(defaultRules);
+
 export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
+  // @todo: #4.1 — заполнить выпадающие списки опциями
+  Object.keys(indexes).forEach((indexName) => {
+    if (indexName === 'sellers') {
+        const allOption = document.createElement('option');
+        allOption.value = '';
+        allOption.textContent = 'Все';
 
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+        const options = Object.values(indexes[indexName]).map(name => {
+            const option = document.createElement('option');
+            option.value = name;
+            option.textContent = name;
+            return option;
+        });
 
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        const searchBySellerElement = elements.searchBySeller;
+        searchBySellerElement.appendChild(allOption);
+        options.forEach(option => searchBySellerElement.appendChild(option));
     }
+  });
+
+  return (data, state, action) => {
+    // @todo: #4.2 — обработать очистку поля
+
+    if (action && action.name ==='clear') {
+        action.parentElement.querySelector('input').value = '';
+        state[action.dataset.field] = '';
+    }
+    
+    // @todo: #4.5 — отфильтровать данные используя компаратор
+    return data.filter(row => compare(row, {
+        ...state,
+        total: [state.totalFrom, state.totalTo],
+      }));
+  };
 }
--- src/components/table.js
+++ src/components/table.js
@@ -1,4 +1,4 @@
-import {cloneTemplate} from "../lib/utils.js";
+import { cloneTemplate } from '../lib/utils.js';
 
 /**
  * Инициализирует таблицу и вызывает коллбэк при любых изменениях и нажатиях на кнопки
@@ -8,18 +8,57 @@ import {cloneTemplate} from "../lib/utils.js";
  * @returns {{container: Node, elements: *, render: render}}
  */
 export function initTable(settings, onAction) {
-    const {tableTemplate, rowTemplate, before, after} = settings;
-    const root = cloneTemplate(tableTemplate);
+  const { tableTemplate, rowTemplate, before, after } = settings;
+  const root = cloneTemplate(tableTemplate);
 
-    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+  // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
 
-    // @todo: #1.3 —  обработать события и вызвать onAction()
+  before.reverse().forEach((subName) => {
+    root[subName] = cloneTemplate(subName);
+    root.container.prepend(root[subName].container);
+  });
 
-    const render = (data) => {
-        // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
-        root.elements.rows.replaceChildren(...nextRows);
-    }
+  after.forEach((subName) => {
+    root[subName] = cloneTemplate(subName);
+    root.container.append(root[subName].container);
+  });
 
-    return {...root, render};
+  // @todo: #1.3 —  обработать события и вызвать onAction()
+
+  root.container.addEventListener('change', () => {
+    onAction();
+  });
+
+  root.container.addEventListener('reset', () => {
+    setTimeout(onAction, 100);
+  });
+
+  root.container.addEventListener('submit', (e) => {
+    e.preventDefault();
+    onAction(e.submitter);
+  });
+
+  const render = (data) => {
+    // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
+    const nextRows = data.map((item) => {
+      const row = cloneTemplate(rowTemplate);
+
+      row.elements.date.textContent = item.date;
+      row.elements.customer.textContent = item.customer;
+      row.elements.seller.textContent = item.seller;
+      row.elements.total.textContent = item.total;
+
+      Object.keys(item).forEach((key) => {
+        if (key in row.elements) {
+          row.elements[key].textContent = item[key];
+        }
+      });
+
+      return row.container;
+    });
+
+    root.elements.rows.replaceChildren(...nextRows);
+  };
+
+  return { ...root, render };
 }
